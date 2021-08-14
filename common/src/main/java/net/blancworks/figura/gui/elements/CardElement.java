bd7ea02aa3544b85fb4b09f00d631bd47bee6f03
Francy-chan <40085587+Francy-chan@users.noreply.github.com>
2021-08-14 10:33:01 -0300

    cards

--- common/src/main/java/net/blancworks/figura/gui/elements/CardElement.java
+++ common/src/main/java/net/blancworks/figura/gui/elements/CardElement.java
@@ -6,8 +6,10 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.*;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.util.math.Vector3d;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.RabbitEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3f;
@@ -15,20 +17,36 @@ import org.lwjgl.opengl.GL11;
 
 public class CardElement extends StencilElement {
 
-    public Identifier textureID = new Identifier("textures/misc/forcefield.png");
+    public final Identifier BACKGROUND;
+    public final Identifier BACKGROUND_OVERLAY = new Identifier("figura", "textures/cards/background_overlay.png");
+    public final Text NAME;
+    public final Text AUTHOR;
 
-    private float f = 0;
+    public MinecraftClient client;
 
-    public CardElement() {
+    public CardElement(CardBackground background, Text name, Text author) {
+        this.BACKGROUND = new Identifier(background.namespace, background.path);
+        this.NAME = name;
+        this.AUTHOR = author;
 
-
+        this.client = MinecraftClient.getInstance();
     }
 
+    public enum CardBackground {
+        BLUE("figura", "textures/cards/backgrounds/blue.png"),
+        CLOUDS("figura", "textures/cards/backgrounds/clouds.png");
+
+        public final String namespace;
+        public final String path;
+        CardBackground(String namespace, String path) {
+            this.namespace = namespace;
+            this.path = path;
+        }
+    }
 
     @Override
     public void render(MatrixStack matrixStack, int i, int j, float f) {
 
-        this.f += f;
         matrixStack.push();
 
         float mouseX = (float) MinecraftClient.getInstance().mouse.getX();
@@ -36,16 +54,16 @@ public class CardElement extends StencilElement {
         mouseX -= MinecraftClient.getInstance().getWindow().getWidth() / 2.0f;
         mouseY -= MinecraftClient.getInstance().getWindow().getHeight() / 2.0f;
 
-        matrixStack.translate(150, 150, 0);
-        matrixStack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion((-mouseY / MinecraftClient.getInstance().getWindow().getHeight()) * 80));
-        matrixStack.multiply(Vec3f.NEGATIVE_Y.getDegreesQuaternion((-mouseX / MinecraftClient.getInstance().getWindow().getWidth()) * 80));
+        matrixStack.translate(100, 100, 0);
+        matrixStack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion((-mouseY / MinecraftClient.getInstance().getWindow().getHeight()) * 120));
+        matrixStack.multiply(Vec3f.NEGATIVE_Y.getDegreesQuaternion((-mouseX / MinecraftClient.getInstance().getWindow().getWidth()) * 120));
 
         try {
 
             stencilLayerID = 35;
 
             //Draw the main body of the card
-            /**{
+            /*{
              MinecraftClient.getInstance().getTextureManager().bindTexture(textureID);
 
              BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
@@ -59,28 +77,23 @@ public class CardElement extends StencilElement {
              bufferBuilder.end();
              RenderSystem.enableAlphaTest();
              BufferRenderer.draw(bufferBuilder);
-             }**/
+             }*/
 
             RenderSystem.disableBlend();
             RenderSystem.defaultBlendFunc();
             RenderSystem.enableDepthTest();
-
+            RenderSystem.setShader(GameRenderer::getPositionColorShader);
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0f);
 
             //Prepare stencil by drawing an object where we want the card "viewport" to be
             {
-                MinecraftClient.getInstance().getTextureManager().bindTexture(textureID);
-
-                RenderSystem.setShader(GameRenderer::getPositionColorShader);
-                //RenderSystem.setShaderTexture(0, textureID);
-                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0f);
-
                 BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
                 bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
 
-                bufferBuilder.vertex(matrixStack.peek().getModel(), -25, 25, 0).color(255, 0, 0, 255).texture(0, 1).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 25, 25, 0).color(255, 0, 0, 255).texture(1, 1).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 25, -25, 0).color(255, 0, 0, 255).texture(1, 0).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), -25, -25, 0).color(255, 0, 0, 255).texture(0, 0).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 0, 96, 0).color(255, 0, 0, 255).texture(0, 1).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 64, 96, 0).color(255, 0, 0, 255).texture(1, 1).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 64, 0, 0).color(255, 0, 0, 255).texture(1, 0).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 0, 0, 0).color(255, 0, 0, 255).texture(0, 0).next();
 
                 bufferBuilder.end();
                 setupStencilPrep();
@@ -89,32 +102,52 @@ public class CardElement extends StencilElement {
 
             //Draw the card art here
             {
-                MinecraftClient.getInstance().getTextureManager().bindTexture(textureID);
+                //render bg
+                RenderSystem.setShaderTexture(0, BACKGROUND);
+                drawTexture(matrixStack, 0, 0, 64, 96, 0, 0, 64, 96, 64, 96);
 
-                RenderSystem.setShader(GameRenderer::getPositionColorShader);
-                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0f);
-
+                //stencil magic
                 BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
                 bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
 
-                bufferBuilder.vertex(matrixStack.peek().getModel(), -500, 500, -30).color(0, 255, 0, 255).texture(0, 1).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 500, 500, -30).color(0, 255, 0, 255).texture(1, 1).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 500, -500, -30).color(0, 255, 0, 255).texture(1, 0).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), -500, -500, -30).color(0, 255, 0, 255).texture(0, 0).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 0, 96, 0).color(0, 255, 0, 255).texture(0, 1).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 64, 96, 0).color(0, 255, 0, 255).texture(1, 1).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 64, 0, 0).color(0, 255, 0, 255).texture(1, 0).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 0, 0, 0).color(0, 255, 0, 255).texture(0, 0).next();
 
                 bufferBuilder.end();
                 GlStateManager._clear(GL11.GL_DEPTH_BUFFER_BIT, false);
-                //RenderSystem.disableDepthTest();
                 setupStencil();
                 BufferRenderer.draw(bufferBuilder);
-
                 RenderSystem.enableDepthTest();
 
+                //render overlay
+                RenderSystem.enableBlend();
+
+                RenderSystem.setShaderTexture(0, BACKGROUND_OVERLAY);
+                drawTexture(matrixStack, 0, 0, 64, 96, 0, 0, 64, 96, 64, 96);
+
+                RenderSystem.disableBlend();
+
+                //render text
+                matrixStack.push();
+                matrixStack.translate(3f, 3f,0f); //3px offset
+                drawTextWithShadow(matrixStack, client.textRenderer, NAME, 0, 0, 0xFFFFFF);
+                matrixStack.pop();
+
+                matrixStack.push();
+                matrixStack.translate(3f, 11f,0f); //3px offset + 7px above text + 1px spacing
+                matrixStack.scale(0.75f, 0.75f,1f);
+                drawTextWithShadow(matrixStack, client.textRenderer, AUTHOR, 0, 0, 0xFFFFFF);
+                matrixStack.pop();
             }
 
-            matrixStack.translate(0,30,-15);
-            drawEntity(0, 0, 30, 0, 0, MinecraftClient.getInstance().player, matrixStack);
+            //render model
+            matrixStack.translate(0,0,-15);
+            RabbitEntity entity = new RabbitEntity(EntityType.RABBIT, client.world);
+            entity.setRabbitType(99);
 
+            drawEntity(32, 48, 30, 0, 0, entity, matrixStack);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -132,14 +165,14 @@ public class CardElement extends StencilElement {
 
     }
 
-    public static void drawEntity(int i, int j, int k, float f, float g, LivingEntity livingEntity, MatrixStack matrixStack) {
-        float h = (float)Math.atan((double)(f / 40.0F));
-        float l = (float)Math.atan((double)(g / 40.0F));
+    public static void drawEntity(int x, int y, int scale, float yaw, float pitch, LivingEntity livingEntity, MatrixStack matrixStack) {
+        float h = (float)Math.atan(yaw / 40.0F);
+        float l = (float)Math.atan(pitch / 40.0F);
         matrixStack.push();
-        matrixStack.translate((double)i, (double)j, 0);
+        matrixStack.translate(x, y, 0);
         matrixStack.scale(1.0F, 1.0F, -1.0F);
         matrixStack.translate(0.0D, 0.0D, 0.0D);
-        matrixStack.scale((float)k, (float)k, (float)k);
+        matrixStack.scale((float) scale, (float) scale, (float) scale);
         Quaternion quaternion = Vec3f.POSITIVE_Z.getDegreesQuaternion(180.0F);
         Quaternion quaternion2 = Vec3f.POSITIVE_X.getDegreesQuaternion(l * 20.0F);
         quaternion.hamiltonProduct(quaternion2);
@@ -160,9 +193,7 @@ public class CardElement extends StencilElement {
         entityRenderDispatcher.setRotation(quaternion2);
         entityRenderDispatcher.setRenderShadows(false);
         VertexConsumerProvider.Immediate immediate = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers();
-        RenderSystem.runAsFancy(() -> {
-            entityRenderDispatcher.render(livingEntity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, matrixStack, immediate, 15728880);
-        });
+        RenderSystem.runAsFancy(() -> entityRenderDispatcher.render(livingEntity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, matrixStack, immediate, 15728880));
         immediate.draw();
         entityRenderDispatcher.setRenderShadows(true);
         livingEntity.bodyYaw = m;
