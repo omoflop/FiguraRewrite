7af570674de8f1a3002f083daff3179793957677
Zandra <aaron.mich.mills@gmail.com>
2021-08-13 19:19:21 -0600

    Add player rendering for testing into the card rendering

--- common/src/main/java/net/blancworks/figura/gui/elements/CardElement.java
+++ common/src/main/java/net/blancworks/figura/gui/elements/CardElement.java
@@ -1,12 +1,17 @@
 package net.blancworks.figura.gui.elements;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.*;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3d;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3f;
+import org.lwjgl.opengl.GL11;
 
 public class CardElement extends StencilElement {
 
@@ -31,10 +36,9 @@ public class CardElement extends StencilElement {
         mouseX -= MinecraftClient.getInstance().getWindow().getWidth() / 2.0f;
         mouseY -= MinecraftClient.getInstance().getWindow().getHeight() / 2.0f;
 
-        matrixStack.translate(50, 75, 0);
-        matrixStack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion((mouseY / MinecraftClient.getInstance().getWindow().getHeight()) * 80));
-        matrixStack.multiply(Vec3f.NEGATIVE_Y.getDegreesQuaternion((mouseX / MinecraftClient.getInstance().getWindow().getWidth()) * 80));
-        matrixStack.translate(-50, -75, 0);
+        matrixStack.translate(150, 150, 0);
+        matrixStack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion((-mouseY / MinecraftClient.getInstance().getWindow().getHeight()) * 80));
+        matrixStack.multiply(Vec3f.NEGATIVE_Y.getDegreesQuaternion((-mouseX / MinecraftClient.getInstance().getWindow().getWidth()) * 80));
 
         try {
 
@@ -73,10 +77,10 @@ public class CardElement extends StencilElement {
                 BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
                 bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
 
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 10, 150, 0).color(255, 0, 0, 255).texture(0, 1).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 90, 150, 0).color(255, 0, 0, 255).texture(1, 1).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 90, 10, 0).color(255, 0, 0, 255).texture(1, 0).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 10, 10, 0).color(255, 0, 0, 255).texture(0, 0).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), -25, 25, 0).color(255, 0, 0, 255).texture(0, 1).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 25, 25, 0).color(255, 0, 0, 255).texture(1, 1).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 25, -25, 0).color(255, 0, 0, 255).texture(1, 0).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), -25, -25, 0).color(255, 0, 0, 255).texture(0, 0).next();
 
                 bufferBuilder.end();
                 setupStencilPrep();
@@ -93,16 +97,24 @@ public class CardElement extends StencilElement {
                 BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
                 bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
 
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 20, 120, 15).color(0, 255, 0, 255).texture(0, 1).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 80, 120, 15).color(0, 255, 0, 255).texture(1, 1).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 80, 20, 15).color(0, 255, 0, 255).texture(1, 0).next();
-                bufferBuilder.vertex(matrixStack.peek().getModel(), 20, 20, 15).color(0, 255, 0, 255).texture(0, 0).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), -500, 500, -30).color(0, 255, 0, 255).texture(0, 1).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 500, 500, -30).color(0, 255, 0, 255).texture(1, 1).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), 500, -500, -30).color(0, 255, 0, 255).texture(1, 0).next();
+                bufferBuilder.vertex(matrixStack.peek().getModel(), -500, -500, -30).color(0, 255, 0, 255).texture(0, 0).next();
 
                 bufferBuilder.end();
-                RenderSystem.disableDepthTest();
+                GlStateManager._clear(GL11.GL_DEPTH_BUFFER_BIT, false);
+                //RenderSystem.disableDepthTest();
                 setupStencil();
                 BufferRenderer.draw(bufferBuilder);
+
+                RenderSystem.enableDepthTest();
+
             }
+
+            matrixStack.translate(0,30,-15);
+            drawEntity(0, 0, 30, 0, 0, MinecraftClient.getInstance().player, matrixStack);
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -120,5 +132,45 @@ public class CardElement extends StencilElement {
 
     }
 
-
+    public static void drawEntity(int i, int j, int k, float f, float g, LivingEntity livingEntity, MatrixStack matrixStack) {
+        float h = (float)Math.atan((double)(f / 40.0F));
+        float l = (float)Math.atan((double)(g / 40.0F));
+        matrixStack.push();
+        matrixStack.translate((double)i, (double)j, 0);
+        matrixStack.scale(1.0F, 1.0F, -1.0F);
+        matrixStack.translate(0.0D, 0.0D, 0.0D);
+        matrixStack.scale((float)k, (float)k, (float)k);
+        Quaternion quaternion = Vec3f.POSITIVE_Z.getDegreesQuaternion(180.0F);
+        Quaternion quaternion2 = Vec3f.POSITIVE_X.getDegreesQuaternion(l * 20.0F);
+        quaternion.hamiltonProduct(quaternion2);
+        matrixStack.multiply(quaternion);
+        float m = livingEntity.bodyYaw;
+        float n = livingEntity.getYaw();
+        float o = livingEntity.getPitch();
+        float p = livingEntity.prevHeadYaw;
+        float q = livingEntity.headYaw;
+        livingEntity.bodyYaw = 180.0F + h * 20.0F;
+        livingEntity.setYaw(180.0F + h * 40.0F);
+        livingEntity.setPitch(-l * 20.0F);
+        livingEntity.headYaw = livingEntity.getYaw();
+        livingEntity.prevHeadYaw = livingEntity.getYaw();
+        DiffuseLighting.method_34742();
+        EntityRenderDispatcher entityRenderDispatcher = MinecraftClient.getInstance().getEntityRenderDispatcher();
+        quaternion2.conjugate();
+        entityRenderDispatcher.setRotation(quaternion2);
+        entityRenderDispatcher.setRenderShadows(false);
+        VertexConsumerProvider.Immediate immediate = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers();
+        RenderSystem.runAsFancy(() -> {
+            entityRenderDispatcher.render(livingEntity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, matrixStack, immediate, 15728880);
+        });
+        immediate.draw();
+        entityRenderDispatcher.setRenderShadows(true);
+        livingEntity.bodyYaw = m;
+        livingEntity.setYaw(n);
+        livingEntity.setPitch(o);
+        livingEntity.prevHeadYaw = p;
+        livingEntity.headYaw = q;
+        matrixStack.pop();
+        DiffuseLighting.enableGuiDepthLighting();
+    }
 }
